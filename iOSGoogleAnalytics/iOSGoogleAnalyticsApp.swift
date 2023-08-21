6f75b5bdf172c34f140afd44bd14f453a2ada985
ckyeon <qsky8520@gmail.com>
2023-08-21 21:48:15 +0900

    initial commit

---
+++ iOSGoogleAnalytics/iOSGoogleAnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSGoogleAnalyticsApp.swift
+//  iOSGoogleAnalytics
+//
+//  Created by 조기연 on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSGoogleAnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
