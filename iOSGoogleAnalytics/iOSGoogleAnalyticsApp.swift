7c5e994da6257a27747e8bc40643398b86dee58c
ckyeon <qsky8520@gmail.com>
2023-08-31 03:54:58 +0900

    apply google analytics

--- iOSGoogleAnalytics/iOSGoogleAnalyticsApp.swift
+++ iOSGoogleAnalytics/iOSGoogleAnalyticsApp.swift
@@ -5,10 +5,15 @@
 //  Created by 조기연 on 2023/08/21.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct iOSGoogleAnalyticsApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
